0855cdca16370f1d002b0a2aa85fecc72ca0a35f
cemartins <carlos.mfa.martins@gmail.com>
2013-10-28 08:47:46 +0000

    Packaging for osx done

--- src/main/java/net/sf/juffrou/mq/ui/Main.java
+++ src/main/java/net/sf/juffrou/mq/ui/Main.java
@@ -1,5 +1,8 @@
 package net.sf.juffrou.mq.ui;
 
+import java.io.File;
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -7,29 +10,58 @@ import javafx.stage.Stage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main extends Application implements ConsolePreloader.SharedScene {
 
+	static {
+		if (System.getProperty("os.name").startsWith("Mac"))
+			System.setProperty("mq.console.log.dir", System.getProperty("user.home") + "/Library/Application Support/MQConsole");
+		else
+			System.setProperty("mq.console.log.dir", System.getenv("APPDATA") + "/MQConsole");
+	}
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 	public static ApplicationContext applicationContext;
 
 	private Parent parentNode;
 
 	public static void main(String[] args) {
-		System.setProperty("mq.console.dir", ".");
+		System.setProperty("mq.console.dir", System.getProperty("user.dir") + File.separator);
 		launch(args);
+	}
+	
+	private static void setMQConsoleDir() {
+		String mqConsoleDir = System.getProperty("user.dir") + File.separator;
+		if(System.getProperty("os.name").startsWith("Mac")) {
+			URL mySource = Main.class.getProtectionDomain().getCodeSource().getLocation();
+			mqConsoleDir= mySource.getPath().substring(0, mySource.getPath().lastIndexOf('/'));
+		}
+		System.setProperty("mq.console.dir", mqConsoleDir);
 	}
 
 	@Override
 	public void init() {
+		if(System.getProperty("mq.console.dir") == null)
+			setMQConsoleDir();
 		if(log.isDebugEnabled()) {
 			log.debug("Console started");
-			log.debug("Working Directory=" + System.getProperty("user.dir"));
+			log.debug("user.dir=" + System.getProperty("user.dir"));
+			log.debug("mq.console.dir=" + System.getProperty("mq.console.dir"));
+			log.debug("mq.console.log.dir=" + System.getProperty("mq.console.log.dir"));
 		}
-		applicationContext = new ClassPathXmlApplicationContext(
-				new String[] { "META-INF/context/mq-console-application-context.xml" });
+		try {
+			applicationContext = new ClassPathXmlApplicationContext(
+					new String[] { "META-INF/context/mq-console-application-context.xml" });
+		}
+		catch(BeansException be) {
+			if(log.isErrorEnabled()) {
+				log.error("Cannot start application.");
+				log.error(be.getMessage());
+			}
+			throw be;	
+		}
 		if(log.isDebugEnabled())
 			log.debug("Console context loaded");
 		//prepare application scene
